0445e4c0ca54d251ccad2f3556c072f76f5fd823
AugustusCeasar <agustinplazareino@gmail.com>
2026-03-16 20:25:35 +0000

    added functions to generate faiss index and search for neighbors

--- semantic_search/semantic_utils.py
+++ semantic_search/semantic_utils.py
@@ -2,6 +2,9 @@ import json
 
 from sentence_transformers import SentenceTransformer
 
+import numpy as np
+import faiss
+
 DEFAULT_SENCENCE_ENCODING_MODEL = "sentence-transformers/embeddinggemma-300m-medical"
 
 
@@ -35,11 +38,40 @@ def load_data(fn):
     return data
 
 
+index_singleton = None
+
+
+def get_semantic_index(embedding_data, singleton_refresh=False):
+    global index_singleton
+    if index_singleton is None or singleton_refresh:
+        index_singleton = faiss.IndexFlatL2(len(embedding_data[0]["embedding"]))
+
+        for item in embedding_data:
+            index_singleton.add(np.array([item["embedding"]]))
+
+    return index_singleton
+
+
+def get_k_nearest_neighbors(query, semantic_index, k=5, model_name=None):
+    model = SentenceTransformer(model_name or DEFAULT_SENCENCE_ENCODING_MODEL)
+
+    query_embeddings = model.encode_query(query)
+
+    distance, indices = semantic_index.search(np.expand_dims(query_embeddings, axis=0), k=k)
+
+    return distance, indices
+
+
 def debug():
     embeddings_direct = get_embeddings(save_fn="data/name_encoding.json")
     print(embeddings_direct)
     embeddings_loaded = load_data("data/name_encoding.json")
     print(embeddings_loaded)
+    test_index = get_semantic_index(embeddings_direct)
+    D, I = get_k_nearest_neighbors(query="my research goal is to study heartrate variations in icu patients",
+                                   semantic_index=test_index)
+    print(D)
+    print(I)
 
 
 if __name__ == "__main__":
